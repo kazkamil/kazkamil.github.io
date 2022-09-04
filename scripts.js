75492f463ea54d6975ba424382654b8515411362
kazkamil <112205904+kazkamil@users.noreply.github.com>
2022-09-04 12:46:41 +0200

    java integration

---
+++ scripts.js
@@ -0,0 +1,13 @@
+
+<script>
+// Code for Safari 3.1 to 6.0
+document.getElementById("mySidenav").addEventListener("webkitTransitionEnd", myFunction);
+
+// Standard syntax
+document.getElementById("mySidenav").addEventListener("transitionend", myFunction);
+
+function myFunction() {
+  this.innerHTML = "transitionend event occured - The transition has completed";
+  this.style.backgroundColor = "pink";
+}
+</script>
